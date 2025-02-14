c659ff6064e134efb81ed90241a93f4bae69baeb
William Capt <wcapt@c1r13s2.42lausanne.ch>
2025-02-14 15:24:49 +0100

    Minitalk7

--- server.c
+++ server.c
@@ -6,36 +6,61 @@
 /*   By: wcapt < wcapt@student.42lausanne.ch >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 14:07:38 by wcapt             #+#    #+#             */
-/*   Updated: 2025/02/13 16:30:23 by wcapt            ###   ########.fr       */
+/*   Updated: 2025/02/14 15:24:18 by wcapt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/includes/libft.h"
 
-#include <unistd.h>
-#include <stdio.h>
+static char	g_temp[2] = {0, 0};
+
+static void	put_in_string(unsigned char m, char **dest)
+{
+	char		*new_dest;
+
+	if (m == '\0')
+	{
+		ft_putstr_fd(*dest, 1);
+		ft_putchar_fd('\n', 1);
+		free(*dest);
+		*dest = NULL;
+	}
+	else
+	{
+		g_temp[0] = m;
+		if (!*dest)
+			*dest = ft_strdup(g_temp);
+		else
+		{
+			new_dest = ft_strjoin(*dest, g_temp);
+			free(dest);
+			*dest = new_dest;
+		}
+	}
+}
 
 void	get_signal(int signal, siginfo_t *info, void *context)
 {
 	static int				bit = 0;
 	static unsigned char	m = 0;
 	pid_t					pid_client;
+	static char				*dest;
 
 	(void)context;
 	pid_client = info->si_pid;
-	if (signal == SIGUSR2)
-		m |= (1 << (7 - bit));
+	m = (m << 1) | (signal == SIGUSR2);
+	//if (signal == SIGUSR2)
+	//	m |= (1 << (7 - bit));
 	bit++;
 	if (bit == 8)
 	{
-		if (m == 0)
+		put_in_string(m, &dest);
+		if (m == '\0')
 		{
-			ft_printf("\n");
+			ft_printf("%s\n", dest);
 			if (kill(pid_client, SIGUSR2) == -1)
 				ft_printf("Error with the confirmation message.\n");
 		}
-		else
-			ft_printf("%c", m);
 		bit = 0;
 		m = 0;
 	}
